4daf8710001d3a7a05881f10534f84df24f589fb
Alex Hicks <tiin57@gmail.com>
2016-03-23 11:06:33 -0400

    Convert WebServer to a class

--- lib/WebServer.ts
+++ lib/WebServer.ts
@@ -1,12 +1,20 @@
 import * as site from "./autoload";
 import * as express from "express";
 
-var app : express.Application = express();
-
-app.get("/", (req : express.Request, res : express.Response) => {
-    res.send("Hello world!");
-});
-
-app.listen(3000, () => {
-    console.log("Example app listening on port 3000!");
-});
+export class WebServer {
+    public app : express.Application = express();
+    public constructor() {
+        this.app = express();
+        this.configure();
+    }
+    public configure() : void {
+        this.app.get("/", (req : express.Request, res : express.Response) => {
+            res.send("Hello world!");
+        });
+    }
+    public start() : void {
+        this.app.listen(3000, () => {
+            console.log("Example app listening on port 3000!");
+        });
+    }
+}
